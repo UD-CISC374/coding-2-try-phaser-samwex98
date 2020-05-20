34986f60752c3fbd09e83faaa442f38a0e6e9c06
Sam.wex98 <samwex@udel.edu>
2020-05-20 02:57:44 -0400

    Added overlap functionality and Game Over Screen

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,15 +1,17 @@
 import ExampleObject from '../objects/exampleObject';
+import { GameObjects } from 'phaser';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
   background: Phaser.GameObjects.TileSprite;
-  ship1: Phaser.GameObjects.Sprite;
-  ship2: Phaser.GameObjects.Sprite;
-  ship3: Phaser.GameObjects.Sprite;
+  ship1: Phaser.Physics.Arcade.Sprite;
+  ship2: Phaser.Physics.Arcade.Sprite;
+  ship3: Phaser.Physics.Arcade.Sprite;
   player: Phaser.Physics.Arcade.Sprite;
   cursorKeys: any;
   scoreLabel: Phaser.GameObjects.BitmapText;
   score: number;
+  explosion: Phaser.GameObjects.Sprite;
   explosionSound: Phaser.Sound.BaseSound;
   music: Phaser.Sound.BaseSound;
 
@@ -18,17 +20,19 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
-    this.background = this.add.tileSprite(0, 0, this.sys.game.canvas.width, 
+    this.background = this.add.tileSprite(0, 0, this.sys.game.canvas.width,
       this.sys.game.canvas.height, "background");
-      this.background.setOrigin(0, 0);
+    this.background.setOrigin(0, 0);
 
-    this.ship1 = this.add.sprite(this.scale.width, this.scale.height/3, "ship");
-    this.ship2 = this.add.sprite(this.scale.width, this.scale.height/2, "ship2");
-    this.ship3 = this.add.sprite(this.scale.width, this.scale.height/4, "ship3")
+    this.explosion = this.add.sprite(this.scale.width, this.scale.height, "explosion");
+    this.explosion.visible = false;
+    this.ship1 = this.physics.add.sprite(this.scale.width, this.scale.height / 3, "ship");
+    this.ship2 = this.physics.add.sprite(this.scale.width, this.scale.height / 2, "ship2");
+    this.ship3 = this.physics.add.sprite(this.scale.width, this.scale.height / 4, "ship3")
 
-
-
-    
+    this.ship1.setGravityY(-400);
+    this.ship2.setGravityY(-400);
+    this.ship3.setGravityY(-400);
 
     this.ship1.play("ship1_anim");
     this.ship2.play("ship2_anim");
@@ -37,12 +41,6 @@ export default class MainScene extends Phaser.Scene {
     this.ship1.rotation = 1.56;
     this.ship2.rotation = 1.56;
     this.ship3.rotation = 1.56;
-
-    this.ship1.setInteractive();
-    this.ship2.setInteractive();
-    this.ship3.setInteractive();
-
-    this.input.on('gameobjectdown', this.destroyShip, this);
 
     this.score = 0;
     this.scoreLabel = this.add.bitmapText(10, 5, "pixelFont", "SCORE", 16);
@@ -64,23 +62,29 @@ export default class MainScene extends Phaser.Scene {
 
     this.exampleObject = new ExampleObject(this, 0, 0);
 
-    this.player = this.physics.add.sprite(this.scale.width/2 - 8, this.scale.height - 64, "player");
+    this.player = this.physics.add.sprite(this.scale.width / 2 - 8, this.scale.height - 64, "player");
     this.player.setScale(0.25);
     this.player.play("run");
+
     this.cursorKeys = this.input.keyboard.createCursorKeys();
     this.player.setCollideWorldBounds(true);
-
-    this.physics.overlap(this.player, this.ship1, this.checker);
 
   }
 
   checker() {
-    console.log("overlap");
+    this.explosion.x = this.player.x;
+    this.explosion.y = this.player.y;
+    this.player.alpha = 0;
+    this.explosion.visible = true;
+    this.explosion.play("explode");
+    this.explosion.destroy();
+    let gameoverText = this.add.text(this.scale.width/2, this.scale.height/2, 'Game Over');
+    gameoverText.setDepth(1);
   }
 
   zeroPad(number, size) {
     var stringNumber = String(number);
-    while(stringNumber.length , (size || 2)) {
+    while (stringNumber.length, (size || 2)) {
       stringNumber = "0" + stringNumber;
     }
     return stringNumber;
@@ -102,31 +106,31 @@ export default class MainScene extends Phaser.Scene {
 
   moveShip(ship, speed) {
     ship.x -= speed;
-    if(ship.x < 0) {
+    if (ship.x < 0) {
       this.resetShipPos(ship);
     }
   }
 
-  moveShip3(ship, speed){
+  moveShip3(ship, speed) {
     ship.x -= speed;
-    if(ship.x > this.sys.game.canvas.width-7){
+    if (ship.x > this.sys.game.canvas.width - 7) {
       ship.visible = false;
     }
-    else{
+    else {
       ship.visible = true;
     }
-    if(ship.x < 0){
+    if (ship.x < 0) {
       this.time.addEvent({
         delay: 5000,
         callback: () =>
-        this.resetShipPos(ship)
+          this.resetShipPos(ship)
       });
     }
   }
 
   resetShipPos(ship) {
     ship.x = this.sys.game.canvas.width;
-    var randomy = Phaser.Math.Between(this.sys.game.canvas.height/2, this.sys.game.canvas.height - 20);
+    var randomy = Phaser.Math.Between(this.sys.game.canvas.height / 2, this.sys.game.canvas.height - 20);
     ship.y = randomy;
   }
 
@@ -134,26 +138,21 @@ export default class MainScene extends Phaser.Scene {
     this.player.setVelocityY(100);
   }
 
-  destroyShip(pointer, gameObject) {
-    gameObject.setTexture("explosion");
-    gameObject.play("explode");
-  }
-
   movePlayerManager() {
-    if(this.cursorKeys.left.isDown) {
+    if (this.cursorKeys.left.isDown) {
       this.player.setVelocityX(-200);
     }
-    else if(this.cursorKeys.right.isDown) {
+    else if (this.cursorKeys.right.isDown) {
       this.player.setVelocityX(200);
     }
     else {
       this.player.setVelocityX(0);
     }
 
-    if(this.cursorKeys.up.isDown && this.player.y == 253.25){
+    if (this.cursorKeys.up.isDown && this.player.y == 253.25) {
       this.player.setVelocityY(-250);
     }
-    if(this.cursorKeys.down.isDown) {
+    if (this.cursorKeys.down.isDown) {
       this.player.setVelocityY(200);
     }
   }
@@ -165,6 +164,9 @@ export default class MainScene extends Phaser.Scene {
 
     this.background.tilePositionX += 0.5;
     this.movePlayerManager();
-
+    
+    this.physics.overlap(this.player, this.ship1, this.checker, undefined, this);
+    this.physics.overlap(this.player, this.ship2, this.checker, undefined, this);
+    this.physics.overlap(this.player, this.ship3, this.checker, undefined, this);
   }
 }
