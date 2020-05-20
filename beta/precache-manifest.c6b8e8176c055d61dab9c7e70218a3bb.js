3217525768aeeaa5ff370d42ca9462ebac96014a
Sam.wex98 <samwex@udel.edu>
2020-05-20 02:56:52 -0400

    Updates

---
+++ beta/precache-manifest.c6b8e8176c055d61dab9c7e70218a3bb.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-phaser-game-template/betaassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/educational-phaser-game-template/betaassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/educational-phaser-game-template/betaassets/font/font.xml"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-phaser-game-template/betaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-phaser-game-template/betaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-phaser-game-template/betaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-phaser-game-template/betaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-phaser-game-template/betaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "3248ec26f149fe02dea41ceb899a407a",
+    "url": "/educational-phaser-game-template/betaassets/images/background_platformer.png"
+  },
+  {
+    "revision": "86b88d1e4615b77a8b94a93ba82500bf",
+    "url": "/educational-phaser-game-template/betaassets/sounds/beam.mp3"
+  },
+  {
+    "revision": "ce33e3b835ac69e0766dbe6d7c67dfc1",
+    "url": "/educational-phaser-game-template/betaassets/sounds/beam.ogg"
+  },
+  {
+    "revision": "ce8dbd56bcb81632df7279e91fdcec54",
+    "url": "/educational-phaser-game-template/betaassets/sounds/explosion.mp3"
+  },
+  {
+    "revision": "4b9be0c2fdf194385275c2e8d006949b",
+    "url": "/educational-phaser-game-template/betaassets/sounds/explosion.ogg"
+  },
+  {
+    "revision": "2101f87289e14406eb4b215cf55a3ebc",
+    "url": "/educational-phaser-game-template/betaassets/sounds/pickup.mp3"
+  },
+  {
+    "revision": "2caf90e1d1576c0a7891bfb9723ded61",
+    "url": "/educational-phaser-game-template/betaassets/sounds/pickup.ogg"
+  },
+  {
+    "revision": "da087af3292fb062136a96eb496ceaf7",
+    "url": "/educational-phaser-game-template/betaassets/sounds/sci-fi_platformer12.mp3"
+  },
+  {
+    "revision": "240502f3ab4e1955dd49c1dbf827e247",
+    "url": "/educational-phaser-game-template/betaassets/sounds/sci-fi_platformer12.ogg"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/educational-phaser-game-template/betaassets/spritesheets/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/educational-phaser-game-template/betaassets/spritesheets/explosion.png"
+  },
+  {
+    "revision": "b70a7978cdc41e84c165d46eed433dce",
+    "url": "/educational-phaser-game-template/betaassets/spritesheets/running_man.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/educational-phaser-game-template/betaassets/spritesheets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/educational-phaser-game-template/betaassets/spritesheets/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/educational-phaser-game-template/betaassets/spritesheets/ship3.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-phaser-game-template/betafavicon.ico"
+  },
+  {
+    "revision": "7075c2b8fcdd5d529865",
+    "url": "/educational-phaser-game-template/betagen.main.33ba4c4f8fa82f9e1de0.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-phaser-game-template/betagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "36ff78bd0bb41e2fbdb93e542dd224da",
+    "url": "/educational-phaser-game-template/betaindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/educational-phaser-game-template/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-phaser-game-template/betasw.js"
+  }
+]);
---
+++ beta/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-phaser-game-template/betaprecache-manifest.c6b8e8176c055d61dab9c7e70218a3bb.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
