80ce40a47f4cbc5cb7cec66ce00a68c460f2f9e9
Sam.wex98 <samwex@udel.edu>
2020-05-20 01:35:44 -0400

    Working on alpha

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,15 +2,169 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
+  background: Phaser.GameObjects.TileSprite;
+  ship1: Phaser.GameObjects.Sprite;
+  ship2: Phaser.GameObjects.Sprite;
+  ship3: Phaser.GameObjects.Sprite;
+  player: Phaser.Physics.Arcade.Sprite;
+  cursorKeys: any;
+  scoreLabel: Phaser.GameObjects.BitmapText;
+  score: number;
+  explosionSound: Phaser.Sound.BaseSound;
+  music: Phaser.Sound.BaseSound;
 
   constructor() {
     super({ key: 'MainScene' });
   }
 
   create() {
+    this.background = this.add.tileSprite(0, 0, this.sys.game.canvas.width, 
+      this.sys.game.canvas.height, "background");
+      this.background.setOrigin(0, 0);
+
+    this.ship1 = this.add.sprite(this.scale.width, this.scale.height/3, "ship");
+    this.ship2 = this.add.sprite(this.scale.width, this.scale.height/2, "ship2");
+    this.ship3 = this.add.sprite(this.scale.width, this.scale.height/4, "ship3")
+
+
+
+    
+
+    this.ship1.play("ship1_anim");
+    this.ship2.play("ship2_anim");
+    this.ship3.play("ship3_anim");
+
+    this.ship1.rotation = 1.56;
+    this.ship2.rotation = 1.56;
+    this.ship3.rotation = 1.56;
+
+    this.ship1.setInteractive();
+    this.ship2.setInteractive();
+    this.ship3.setInteractive();
+
+    this.input.on('gameobjectdown', this.destroyShip, this);
+
+    this.score = 0;
+    this.scoreLabel = this.add.bitmapText(10, 5, "pixelFont", "SCORE", 16);
+
+    this.explosionSound = this.sound.add("audio_explosion");
+    this.music = this.sound.add("music");
+
+    var musicConfig = {
+      mute: false,
+      volume: 1,
+      rate: 1,
+      detune: 0,
+      seek: 0,
+      loop: false,
+      delay: 0
+    }
+    this.music.play(musicConfig);
+
+
     this.exampleObject = new ExampleObject(this, 0, 0);
+
+    this.player = this.physics.add.sprite(this.scale.width/2 - 8, this.scale.height - 64, "player");
+    this.player.setScale(0.25);
+    this.player.play("run");
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    this.player.setCollideWorldBounds(true);
+
+    this.physics.overlap(this.player, this.ship1, this.checker);
+
+  }
+
+  checker() {
+    console.log("overlap");
+  }
+
+  zeroPad(number, size) {
+    var stringNumber = String(number);
+    while(stringNumber.length , (size || 2)) {
+      stringNumber = "0" + stringNumber;
+    }
+    return stringNumber;
+  }
+
+
+
+
+  hitEnemy(projectile, enemy) {
+
+
+    projectile.destroy();
+    this.resetShipPos(enemy);
+    this.score += 15;
+    var scoreFormated = this.zeroPad(this.score, 6);
+    this.scoreLabel.text = "SCORE " + scoreFormated;
+    this.explosionSound.play();
+  }
+
+  moveShip(ship, speed) {
+    ship.x -= speed;
+    if(ship.x < 0) {
+      this.resetShipPos(ship);
+    }
+  }
+
+  moveShip3(ship, speed){
+    ship.x -= speed;
+    if(ship.x > this.sys.game.canvas.width-7){
+      ship.visible = false;
+    }
+    else{
+      ship.visible = true;
+    }
+    if(ship.x < 0){
+      this.time.addEvent({
+        delay: 5000,
+        callback: () =>
+        this.resetShipPos(ship)
+      });
+    }
+  }
+
+  resetShipPos(ship) {
+    ship.x = this.sys.game.canvas.width;
+    var randomy = Phaser.Math.Between(this.sys.game.canvas.height/2, this.sys.game.canvas.height - 20);
+    ship.y = randomy;
+  }
+
+  endJump() {
+    this.player.setVelocityY(100);
+  }
+
+  destroyShip(pointer, gameObject) {
+    gameObject.setTexture("explosion");
+    gameObject.play("explode");
+  }
+
+  movePlayerManager() {
+    if(this.cursorKeys.left.isDown) {
+      this.player.setVelocityX(-200);
+    }
+    else if(this.cursorKeys.right.isDown) {
+      this.player.setVelocityX(200);
+    }
+    else {
+      this.player.setVelocityX(0);
+    }
+
+    if(this.cursorKeys.up.isDown && this.player.y == 253.25){
+      this.player.setVelocityY(-250);
+    }
+    if(this.cursorKeys.down.isDown) {
+      this.player.setVelocityY(200);
+    }
   }
 
   update() {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 2);
+    this.moveShip3(this.ship3, 3);
+
+    this.background.tilePositionX += 0.5;
+    this.movePlayerManager();
+
   }
 }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,9 +4,75 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image("background", "assets/images/background_platformer.png");
+    this.load.spritesheet("ship", "assets/spritesheets/ship.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png", {
+      frameWidth: 32,
+      frameHeight: 16
+    });
+    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png", {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("player", "assets/spritesheets/running_man.png", {
+      frameWidth: 104,
+      frameHeight: 150
+    });
+
+    this.load.bitmapFont("pixelFont", "assets/font/font.png", "assets/font/font.xml");
+
+    this.load.audio("audio_beam", ["assets/sounds/beam.ogg", "assets/sounds/beam.mp3"]);
+    this.load.audio("audio_explosion", ["assets/sounds/explosion.ogg", "assets/sounds/explosion.mp3"]);
+    this.load.audio("music", ["assets/sounds/sci-fi_platformer12.ogg", "assets/sounds/sci-fi_platformer12.mp3"]);
   }
 
   create() {
+    
+    this.add.text(20, 20, "Loading game...");
     this.scene.start('MainScene');
+
+    this.anims.create({
+      key: "ship1_anim",
+      frames: this.anims.generateFrameNumbers("ship", {start: 0, end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });    
+    this.anims.create({
+      key: "ship2_anim",
+      frames: this.anims.generateFrameNumbers("ship2", {start: 0, end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "ship3_anim",
+      frames: this.anims.generateFrameNumbers("ship3", {start: 0, end: 1}),
+      frameRate: 20,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "explode",
+      frames: this.anims.generateFrameNumbers("explosion", {start: 0, end: 4}),
+      frameRate: 20,
+      repeat: 0,
+      hideOnComplete: true
+    });
+    this.anims.create ({
+      key: "run",
+      frames: this.anims.generateFrameNumbers("player", {start: 0, end: 5}),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.anims.create({
+      key: "jump",
+      frames: this.anims.generateFrameNumbers("player", {start: 12, end: 13})
+    });
+
   }
 }
